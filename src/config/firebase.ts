4513af90ab8bbb45051ad06e8bd6aebbb6aec816
Peter <56896988+pparce@users.noreply.github.com>
2025-05-31 14:50:39 +0200

    configurando firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,14 @@
 import * as admin from 'firebase-admin';
+import * as path from 'path';
+
+const serviceAccount = require(path.resolve(__dirname, '../../serviceAccountKey.json'));
 
 if (!admin.apps.length) {
-  admin.initializeApp();
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    admin.firestore().settings({ ignoreUndefinedProperties: true });
 }
 
 export const db = admin.firestore();
